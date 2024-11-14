53aad6219daeeacec8208ed21117fa49524fb1be
RudsonMartins <rudson.botelho@aluno.senai.br>
2024-11-14 17:01:35 -0300

    js

---
+++ Java/Scripts/Aula1.js
@@ -0,0 +1,4 @@
+var numero = Number(prompt("Digite um valor"));
+var antecessor = (numero - 1);
+var  sucessor = (numero + 1);
+alert("O antecesor é "+antecessor+" o numero é: "+numero+" o sucesssor é "+sucessor);
